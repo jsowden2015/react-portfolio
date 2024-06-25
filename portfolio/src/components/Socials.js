8d97bad96d75a27ddf4f41d5ae59626a65848dce
Justin Sowden <justinpsowden@gmail.com>
2024-06-25 00:09:02 -0400

    HREF Fix

--- portfolio/src/components/Socials.js
+++ portfolio/src/components/Socials.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Socials = () => (
   <div className="socials">
-    <a href="#" target="_blank">
+    <a href="https://www.linkedin.com/in/justin-sowden-361005184/" target="_blank">
       <img src="assets/icons/icons8-twitter-circled.gif" alt="Twitter" loading="lazy" className="socicon" />
     </a>
-    <a href="#" target="_blank">
+    <a href="https://www.linkedin.com/in/justin-sowden-361005184/" target="_blank">
       <img src="assets/icons/icons8-instagram.gif" alt="Instagram" loading="lazy" className="socicon" />
     </a>
     <a href="https://www.linkedin.com/in/justin-sowden-361005184/" target="_blank">
