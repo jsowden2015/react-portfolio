84ade7c79812c54575ecd79de9bc4eaefd715e1b
Justin Sowden <justinpsowden@gmail.com>
2024-06-24 23:52:51 -0400

    Updated

---
+++ portfolio/src/components/ScrollToTop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ScrollToTop = () => (
+    <i class="scroll-up" id="scroll-up"
+    ><img
+      src="assets/icons/icons8-upward-arrow.gif"
+      class="socicon up-arrow"
+      alt=""
+  /></i>
+);
+
+export default ScrollToTop;
---
+++ portfolio/src/components/Socials.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Socials = () => (
+  <div className="socials">
+    <a href="#" target="_blank">
+      <img src="assets/icons/icons8-twitter-circled.gif" alt="Twitter" loading="lazy" className="socicon" />
+    </a>
+    <a href="#" target="_blank">
+      <img src="assets/icons/icons8-instagram.gif" alt="Instagram" loading="lazy" className="socicon" />
+    </a>
+    <a href="https://www.linkedin.com/in/justin-sowden-361005184/" target="_blank">
+      <img src="assets/icons/icons8-linkedin-circled.gif" alt="Linkedin" loading="lazy" className="socicon" />
+    </a>
+    <a href="https://github.com/jsowden2015" target="_blank">
+      <img src="assets/icons/icons8-github.gif" alt="Github" className="socicon" />
+    </a>
+  </div>
+);
+
+export default Socials;
