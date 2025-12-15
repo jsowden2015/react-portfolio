65d548db8fce56b4c43bf928c9ff655a15be1e43
JSowden <justinsowden@yahoo.com>
2025-12-15 17:25:48 -0500

    Updated removal of deprecated files

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Components/Header';
 import MainContent from './Components/MainContent';
@@ -6,16 +6,6 @@ import Footer from './Components/Footer';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('portfolio');
-
-  useEffect(() => {
-    // Set background images dynamically
-    const bgElement = document.querySelector('#wrapper > .bg');
-    if (bgElement) {
-      const overlayUrl = process.env.PUBLIC_URL + '/images/overlay.png';
-      const bgUrl = process.env.PUBLIC_URL + '/images/bg.jpg';
-      bgElement.style.backgroundImage = `url("${overlayUrl}"), linear-gradient(0deg, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url("${bgUrl}")`;
-    }
-  }, []);
 
   return (
     <div id="wrapper" className="fade-in">
